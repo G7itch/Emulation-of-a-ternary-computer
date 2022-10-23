ff80ef1b570f09ad301b915c59d3a6dd2a4118c8
Thomas Piercy <86956696+G7itch@users.noreply.github.com>
2022-10-23 15:30:38 +0000

    Added some comments, formatted file

--- ALU.py
+++ ALU.py
@@ -1,5 +1,9 @@
-from dectoter import DecToTer
+########### IMPORTS ###########
+from dectoter import DecToTer##
+###############################
 
+
+############################################################
 def add(one,two,sto):
   file = open("memory.ter", "r")
   content = file.readlines()
@@ -17,18 +21,24 @@ def add(one,two,sto):
   file = open("memory.ter", "w")
   file.writelines(content)
   file.close()
+############################################################
 
 def sub(one,two,sto):
   file = open("memory.ter", "r")
   content = file.readlines()
+  
   num1 = (content[one].strip("\n"))
   num2 = (content[two].strip("\n"))
-  res = (int(num1,3) - int(num2, 3))
+``res = (int(num1,3) - int(num2, 3))
+  
   if abs(res) != res:
-    res = 0
+    res = 0 #If subtraction gives a negative answer revert to "00000000"
+    
   a = str(DecToTer(res))
   a = "0"*(8-len(a)) + a
+  
   file.close()
+  
   file = open("memory.ter", "r")
   content = file.readlines()
   content[sto] = a + "\n" 
@@ -37,3 +47,4 @@ def sub(one,two,sto):
   file = open("memory.ter", "w")
   file.writelines(content)
   file.close()
+#############################################################
